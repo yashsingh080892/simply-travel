fcee15cc387bb8039b0d74e9a4fbb1cedcd3e105
yashsingh080892 <yashaswi.singh@testsigma.com>
2025-04-15 17:31:23 +0530

    Updated modal

--- models/booking-detail-response.model.ts
+++ models/booking-detail-response.model.ts
@@ -4,6 +4,12 @@ export class BookingDetailResponse {
     @serializable
     public id!: number;
 
+    @serializable
+    public name!: string;
+
+    @serializable
+    public email!: string;
+
     deserialize(input: any): this {
         return Object.assign(this, deserialize(BookingDetailResponse, input));
     }
