e1564f40778811d3b2773e29632e29536b9da5ec
Vinay Prasad <vinay.prasad@testsigma.com>
2025-04-16 18:06:44 +0530

    new changes by vinay

--- components/signup.tsx
+++ components/signup.tsx
@@ -130,6 +130,8 @@ const Signup = (props:any) => {
     const [eggean, setEggean] = useState(false);
     const onToggleEggean = () => setEggean(prevCount => !prevCount);
 
+    const [eggean, setEggean] = useState(false);
+    const onToggleEggean = () => setEggean(prevCount => !prevCount);
 
     const signUpData:SignUp = new SignUp();
     signUpData.firstName = firstName;
